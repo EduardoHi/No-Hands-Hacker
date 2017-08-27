48774b48ea1426db6dab1f35581bd1ff4504741f
Eduardo Hi <ehidalgo@bluepeople.com>
2017-08-27 09:28:38 -0500

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import {MdButtonModule} from '@angular/material';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked{
   title = 'app';
 
-  onThemeChange(){
+  ngAfterViewChecked(){
     var target_obj = document.getElementsByClassName('ace_scroller')[0];
     var color = getComputedStyle(target_obj).backgroundColor;
+    console.log(color);
   }
 }
--- src/models/File.ts
+++
@@ -1,4 +0,0 @@
-export class File {
-    text: string;
-    name: string;
-}
